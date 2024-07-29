756d31a7cefa9b20a4e5fec0145b5b2de16c22a2
william oliveira <will.h.o@hotmail.com>
2024-07-29 19:19:56 -0300

    meu primeiro comit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Jornada de Dados")
+
+# Variaveis
+idade = 37
+nome = "William"
+
+# Print
+print(idade)
+
+#soma
+nova_idade = idade + 3
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4]
+
+# metodo
+lista_invertida = lista.reverse()
